0b35ff203c7d2d16f6c45e9de4da968cc43c1179
sehajmahajan <sehajmahajan7654@gmail.com>
2025-01-28 20:08:37 +0530

    introduction to javascript and variables

---
+++ javascript/lecture23/basic.js
@@ -0,0 +1,23 @@
+var color="orange"
+
+console.log(color);
+
+console.log("color is "+ color);
+var num=10
+
+console.log("Type of num ", typeof(num) );
+
+console.log(color + "is worn by ", num, "people");
+var a="hello"
+console.log("Type of hello ", typeof(a));
+
+var b=" world"
+console.log(a+b)
+
+var y="hello  "
+console.log(y.length)
+
+console.log(color, " color is beautiful");
+
+
+console.log("code was ", color);
---
+++ javascript/lecture23/var.js
@@ -0,0 +1,8 @@
+var x=10 
+console.log(x);
+
+
+
+x=20
+
+console.log(x);

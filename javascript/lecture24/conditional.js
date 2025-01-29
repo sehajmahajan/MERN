c45d6c6ca7ca68709e2b1549fafa5ebc0ff1e80c
sehajmahajan <sehajmahajan7654@gmail.com>
2025-01-29 20:24:06 +0530

    done with functions and control statements

---
+++ javascript/lecture24/conditional.js
@@ -0,0 +1,25 @@
+let marks=90 
+if(marks>=33){
+    console.log("Pass");
+}else{
+    console.log("Fail");  
+}
+
+//marks - 75- A, 55- B, 40- C, 33-D , f
+if(marks>=75){
+    console.log("A");
+    let dream="engg"
+    if(dream=="doctor"){
+        console.log("Medical");
+    }else{
+        console.log("Non-medical");
+    }
+}else if(marks>=55){
+    console.log("B")
+}else if(marks>=40){
+    console.log("C")
+}else if(marks>=33){
+    console.log("D");  
+}else{
+    console.log("F");
+}
---
+++ javascript/lecture24/function.js
@@ -0,0 +1,21 @@
+function isEven(num) {
+    if(num%2==0) {
+        console.log("number is even");
+    }
+    else {
+        console.log("number is odd");
+    }
+}
+
+isEven(25);
+
+let x = (num)=> {
+    if(num%2==0) {
+        console.log("number is even");
+    }
+    else {
+        console.log("number is odd");
+    }
+}
+
+x(25);
